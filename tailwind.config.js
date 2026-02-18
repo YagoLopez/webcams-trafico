af1fdc497ca112f37e5836a017db314592538e32
YagoLopez <yago.lopez@gmail.com>
2026-02-19 00:06:11 +0100

    feat: Implement the webcam list feature, including `WebcamCard` and `WebcamsListScreen` components, `WebcamData` type, mock data, and supporting design specifications.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#137fec",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101922",
+      },
+    },
   },
   plugins: [],
 }
